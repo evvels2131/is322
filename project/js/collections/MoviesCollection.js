6c14e9b30bf95702456142db35787f61e8fc26b2
Tom <tgoralczyk1@gmail.com>
2016-04-22 11:45:58 -0400

    Created a collection for movies thats responsible for getting now playing, top rated, and upcoming movies

---
+++ project/js/collections/MoviesCollection.js
@@ -0,0 +1,26 @@
+define([
+  'underscore',
+  'backbone',
+  'models/MovieModel'
+], function(_, Backbone, MovieModel) {
+  var MoviesCollection = Backbone.Collection.extend({
+
+    model: MovieModel,
+
+    initialize: function(options) {
+      this.category = options.category;
+      this.page = options.page;
+    },
+
+    url: function() {
+      return 'http://api.themoviedb.org/3/movie/' + this.category +
+        '?api_key=9253b5607c386360d241ec304c9e1520&page=' + this.page;
+    },
+
+    parse: function(response) {
+      return response.results;
+    }
+  });
+
+  return MoviesCollection;
+});
