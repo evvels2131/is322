9baf1a9caf0b30221f3d0b188857bf34bc765165
Tom <tgoralczyk1@gmail.com>
2016-04-22 12:56:13 -0400

    Created a new model for a movie that will grab details from an API

---
+++ project/js/models/MovieDetailsModel.js
@@ -0,0 +1,23 @@
+define([
+  'underscore',
+  'backbone'
+], function(_, Backbone) {
+
+  var MovieDetailsModel = Backbone.Model.extend({
+
+    initialize: function(options) {
+      this.id = options.id;
+    },
+
+    url: function() {
+      return 'https://api.themoviedb.org/3/movie/' + this.id +
+        '?api_key=9253b5607c386360d241ec304c9e1520&append_to_response=trailers,images,similar';
+    },
+
+    parse: function(response) {
+      return response;
+    }
+  });
+  
+  return MovieDetailsModel;
+});
