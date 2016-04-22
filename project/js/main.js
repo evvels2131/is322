bb4aab3cbd16e8eba246660f7112784d7afbcee0
Tom <tgoralczyk1@gmail.com>
2016-04-21 20:11:41 -0400

    Created a main file thats responsible for configuring all third party scirpts and instantiating a new AppRouter

---
+++ project/js/main.js
@@ -0,0 +1,38 @@
+require.config({
+
+  paths: {
+    'jquery': 'libs/jquery/jquery',
+    'jquerymobile': 'libs/jquerymobile/jquerymobile',
+    'underscore': 'libs/underscore/underscore',
+    'backbone': 'libs/backbone/backbone',
+    'templates': '../templates'
+  },
+
+  // Set the configuration for third party scripts that are not AMD
+  // compatible
+  shim: {
+    'backbone': {
+      'deps': ['underscore', 'jquery'],
+      'exports': 'Backbone'
+    }
+  }
+});
+
+// Includes file dependencies
+require([
+  'jquery',
+  'backbone',
+  'routers/AppRouter'
+], function($, Backbone, AppRouter) {
+  $(document).on('mobileinit', function() {
+    $.mobile.ajaxEnabled = false;
+    $.mobile.linkBindingEnabled = false;
+    $.mobile.hashListeningEnabled = false;
+    $.mobile.pushStateEnabled = false;
+  });
+
+  require(['jquerymobile'], function() {
+    this.router = new AppRouter();
+    Backbone.history.start();
+  });
+});
