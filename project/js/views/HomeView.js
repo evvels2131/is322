6e0141b1712bf0dad0eba71c01ccfed1c6f136fc
Tom <tgoralczyk1@gmail.com>
2016-04-22 11:48:30 -0400

    Setted up the HomeView so that it displays a list of now playing movies

--- project/js/views/HomeView.js
+++ project/js/views/HomeView.js
@@ -2,28 +2,53 @@ define([
   'jquery',
   'underscore',
   'backbone',
-  'models/MovieModel'
-  //'models/MovieModel'
-  //'model/MovieModel'
-  //'text!templates/HomePageView.html'
-], function($, _, Backbone, MovieModel) {
+  'collections/MoviesCollection',
+  'views/movies/MoviesView'
+], function($, _, Backbone, MoviesCollection, MoviesView) {
 
   window.HomeView = Backbone.View.extend({
 
     template: _.template($('#home').html()),
 
     initialize: function() {
-      var movieModel = new MovieModel();
-      console.log(movieModel.toJSON());
-      //var movieModel = new MovieModel();
-      //console.log(movieModel.toJSON());
-      console.log('Heyyy');
+      console.log('HomeView Initialized');
+
+      this.content = 'undefined';
+      this.category = 'now_playing';
+      this.page = 1;
+
+      this.moviesCollection = new MoviesCollection({
+        category: this.category,
+        page: this.page
+      });
+
+      this.moviesView = new MoviesView({
+        collection: this.moviesCollection
+      });
+
+      this.render();
     },
 
     render: function(eventName) {
-      $(this.el).html(this.template());
+      var self = this;
+
+      this.moviesCollection.fetch({
+        success: function(response) {
+          self.content = self.moviesView.render().el.outerHTML;
+          $(self.el).html(self.template({
+            content: self.content
+          }));
+          $(self.el).trigger('create');
+        },
+        error: function(error) {
+          $(self.el).html(self.template({
+            content: self.content
+          }));
+          $(self.el).trigger('create');
+          console.log('Error: ' + error);
+        }
+      });
       return this;
     }
-
   });
 });
