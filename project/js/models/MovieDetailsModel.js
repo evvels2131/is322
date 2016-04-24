d4256a206fd7f30212cfc17da85ad9a9d340c989
Tom <tgoralczyk1@gmail.com>
2016-04-23 23:27:40 -0400

    Getting cast information about a particular movie

--- project/js/models/MovieDetailsModel.js
+++ project/js/models/MovieDetailsModel.js
@@ -11,13 +11,13 @@ define([
 
     url: function() {
       return 'https://api.themoviedb.org/3/movie/' + this.id +
-        '?api_key=9253b5607c386360d241ec304c9e1520&append_to_response=trailers,images,similar';
+        '?api_key=9253b5607c386360d241ec304c9e1520&append_to_response=trailers,images,similar,credits';
     },
 
     parse: function(response) {
       return response;
     }
   });
-  
+
   return MovieDetailsModel;
 });
