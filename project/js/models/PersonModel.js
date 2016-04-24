87372122d02025cca19f771e7b8bee8ccc463e5d
Tom <tgoralczyk1@gmail.com>
2016-04-24 16:34:01 -0400

    Added a person model that will represenet a cast member

---
+++ project/js/models/PersonModel.js
@@ -0,0 +1,23 @@
+define([
+  'underscore',
+  'backbone'
+], function(_, Backbone) {
+
+  var PersonModel = Backbone.Model.extend({
+
+    initialize: function(options) {
+      this.id = options.id;
+    },
+
+    url: function() {
+      return 'http://api.themoviedb.org/3/person/ ' + this.id +
+        '?api_key=9253b5607c386360d241ec304c9e1520&append_to_response=movie_credits';
+    },
+
+    parse: function(response) {
+      return response;
+    }
+  });
+
+  return PersomModel;
+});
