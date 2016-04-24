938e0ffabbb8b7370694d42a9620e290e1f8b82b
Tom <tgoralczyk1@gmail.com>
2016-04-24 17:02:26 -0400

    Added a new class that will displays information about a particular actor

---
+++ project/js/views/PersonView.js
@@ -0,0 +1,59 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'models/PersonModel'
+], function($, _, Backbone, PersonModel) {
+
+  window.PersonView = Backbone.View.extend({
+
+    template: _.template($('#person-details-template').html()),
+
+    initialize: function(options) {
+      console.log('PersonView Initialized');
+      var self = this;
+
+      this.content = 'undefined';
+      this.id = options.id;
+
+      this.model = new PersonModel({
+        id: this.id
+      });
+
+      this.render();
+    },
+
+    render: function() {
+      var self = this;
+
+      var template = _.template($('#show-person-details-template').html(), {});
+
+      this.model.fetch({
+        success: function(response) {
+          self.content = template({
+            biography: self.model.get('biography'),
+            birthday: self.model.get('birthday'),
+            name: self.model.get('name'),
+            place_of_birth: self.model.get('place_of_birth'),
+            profile_image: self.model.get('profile_path'),
+            movie_credits: self.model.get('movie_credits')
+          });
+
+          $(self.el).html(self.template({
+            content: self.content
+          }));
+
+          $(self.el).trigger('create');
+        },
+        error: function(error) {
+          console.log('Error ' + error);
+          $(self.el).html(self.template({
+            content: self.content
+          }));
+        }
+      });
+
+      return this;
+    }
+  });
+});
