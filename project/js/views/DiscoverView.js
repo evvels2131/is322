2489020224b299e65d036544079e700c3963aa51
Tom <tgoralczyk1@gmail.com>
2016-05-02 07:53:31 -0400

    Created a separate function for getting favorite actors and genres from the local storage

--- project/js/views/DiscoverView.js
+++ project/js/views/DiscoverView.js
@@ -17,121 +17,15 @@ define([
       this.content = 'undefined';
       this.favorites_exist = false;
       this.page = 1;
-      var genre_ids = [];
-      var genres_string = '';
-
-      var apiString = getApiParameters('fav-genres', 2);
-      console.log(apiString);
-
-      function getApiParameters(localStorageItem, numOfIds) {
-        console.log(String(localStorageItem));
-        var ls = "'" + localStorageItem + "'";
-        var ls = '"' + localStorageItem + '"';
-        var result = [];
-        var api_string = '';
-        console.log(ls);
-
-        console.log(localStorage.getItem(ls));
-  
-        // Check if the local storage item exists
-        if (localStorage.getItem(ls) != null) {
-          localStorageResult = JSON.parse(localStorage.getItem(ls));
-          console.log(localStorageResult);
-
-          if (localStorageResult.length > 0) {
-            // Pick the amount of ids specified
-            result = [];
-            if (localStorageResult.length > numOfIds) {
-              for (var i = 0; i < numOfIds; i++) {
-                result[i] = localStorageResult[Math.floor(Math.random() * localStorageResult.length)];
-              }
-            } else {
-              for (var i = 0; i < localStorageResult.length; i++) {
-                result[i] = localStorageResult[i];
-              }
-            }
-
-            // Convert the result into an API string
-            for (var i = 0; result.length; i++) {
-              if (result.indexOf(result[i]) < result.length - 1) {
-                api_string = api_string.concat(result[i] + ',');
-              } else {
-                api_string = api_string(concat(result[i]));
-              }
-            }
-          }
-        } else {
-          console.log('tits');
-        }
-        return api_string;
-      }
-
-
-      // Check if genres available in local storage
-      if (localStorage.getItem('fav-genres') != null) {
-        genre_ids = JSON.parse(localStorage.getItem('fav-genres'));
-
-        if (genre_ids.length > 0) {
-          this.favorites_exist = true;
-
-          // If it has multiple genre ids, pick 2 at random for better recommendations
-          var arr_result = [];
-          if (genre_ids.length > 2) {
-            for (var i = 0; i < 2; i++) {
-              arr_result[i] = genre_ids[Math.floor(Math.random() * genre_ids.length)];
-            }
-          } else {
-            for (var i = 0; i < genre_ids.length; i++) {
-              arr_result[i] = genre_ids[i];
-            }
-          }
-
-          // Convert the array into a string for the AJAX request
-          for (var i = 0; i < arr_result.length; i++) {
-            if (arr_result.indexOf(arr_result[i]) < arr_result.length - 1) {
-              genres_string = genres_string.concat(arr_result[i] + ',');
-            } else {
-              genres_string = genres_string.concat(arr_result[i]);
-            }
-          }
-        }
-      }
-
-      var actors_string = '';
-      // Check if there are any actors available
-      if (localStorage.getItem('fav-actors') != null) {
-        var actors = JSON.parse(localStorage.getItem('fav-actors'));
-
-        if (actors.length > 0) {
-          var result = [];
-          if (actors.length > 4) {
-            for (var i = 0; i < 4; i++) {
-              result[i] = actors[Math.floor(Math.random() * actors.length)];
-            }
-          } else {
-            for (var i = 0; i < actors.length; i++) {
-              result[i] = actors[i];
-            }
-          }
-
-          // Convert the array into a string for the API request
-          for (var i = 0; i < result.length; i++) {
-            if (result.indexOf(result[i]) < result.length - 1) {
-              actors_string = actors_string.concat(result[i] + '|');
-            } else {
-              actors_string = actors_string.concat(result[i]);
-            }
-          }
-        }
-      }
-
-      console.log(actors_string);
+      this.favGenresLS = this.getFavoritesLS('fav-genres', 2, ',');
+      this.favActorsLS = this.getFavoritesLS('fav-actors', 4, '|');
 
       // If favorite genres are available in the local storage,
       // get the collection and display it to the user
-      if (this.favorites_exist) {
-        this.genres = genres_string;
-        this.actors = actors_string;
+      if (this.favGenresLS != '' || this.favActorsLS != '') {
+        this.favorites_exist = true;
+        this.genres = this.favGenresLS;
+        this.actors = this.favActorsLS;
 
         this.discoverMoviesCollection = new DiscoverMoviesCollection({
           genres: this.genres,
@@ -175,6 +69,51 @@ define([
       }
 
       return this;
+    },
+
+    getFavoritesLS: function(lsItem, numOfItems, char) {
+        var ls;
+        var api = '';
+        var result = [];
+        
+        switch (lsItem) {
+          case 'fav-genres':
+            ls = localStorage.getItem('fav-genres');
+            break;
+          case 'fav-actors':
+            ls = localStorage.getItem('fav-actors');
+            break;
+        }
+
+        if (ls != null) {
+          lsResults = JSON.parse(ls);
+
+          if (lsResults.length > 0)
+          {
+            // Pick the amount of items specified
+            if (lsResults.length > numOfItems) {
+              for (var i = 0; i < numOfItems; i++) {
+                result[i] = lsResults[Math.floor(Math.random() * lsResults.length)];
+              }
+            } else {
+              for (var i = 0; i < lsResults.length; i++) {
+                result[i] = lsResults[i];
+              }
+            }
+          }
+
+          // Convert it into an appropriate API string
+          for (var i = 0; i < result.length; i++) {
+            if (result.indexOf(result[i]) < result.length - 1) {
+              api = api.concat(result[i] + char);
+              console.log(api);
+            } else {
+              api = api.concat(result[i]);
+              console.log(api);
+            }
+          }
+        }
+        return api;
     }
   });
 });
