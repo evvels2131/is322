22817e21ba89ac96cf150f502cc1fa66779c1f93
Tom <tgoralczyk1@gmail.com>
2016-05-03 07:25:53 -0400

    Checked whether the movie was liked and sets its isFavorite attribute appropriately

--- project/js/views/movies/MovieView.js
+++ project/js/views/movies/MovieView.js
@@ -16,7 +16,15 @@ define([
 
     render: function() {
 
+      // Check if the movie is in favorites then set appropriate values
+      if (this.checkIfFavorite(this.model.get('id'))) {
+        this.model.set('isFavorite', true);
+      } else {
+        this.model.set('isFavorite', false);
+      }
+
       $(this.el).html(this.template({
+        isFavorite: this.model.get('isFavorite'),
         original_title: this.model.get('original_title'),
         poster_path: this.model.get('poster_path'),
         vote_average: this.model.get('vote_average'),
@@ -25,6 +33,24 @@ define([
       }));
 
       return this;
+    },
+
+    checkIfFavorite: function(movie_id) {
+      var isFavorite = false;
+      var movie_array = [];
+
+      // Check the local storage
+      if (localStorage.getItem('fav-movies') == null) {
+        console.log('Local storage not yet set for favorite movies');
+      } else {
+        movie_array = JSON.parse(localStorage.getItem('fav-movies'));
+
+        // Check if movie id is already stored in the favorites
+        if (movie_array.indexOf(movie_id) !== -1) {
+          isFavorite = true;
+        }
+      }
+      return isFavorite;
     }
   });
 
